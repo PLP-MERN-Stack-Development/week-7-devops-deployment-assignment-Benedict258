05fdc4ea119ed6e900023d2e4cba2006b0c95def
ISAAC BENEDICT <isaacbenedict001@gmail.com>
2025-07-16 23:15:25 +0100

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [message, setMessage] = useState("Loading...");
+
+  useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message))
+      .catch(() => setMessage("Failed to fetch API 😢"));
+  }, []);
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>{message}</h1>
+    </div>
+  );
+}
+
+export default App;
